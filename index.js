d5f18c7920bceb40e6170a36e775a54eb328c4c2
paul.martin <paul.y.martin@johnlewis.co.uk>
2020-01-03 18:33:31 +0000

    Add status endpoint

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const cors = require('cors');
-const MPlayer = require('./lib/mplayer');
-
+const Player = require('./lib/player');
+const {getUniqueTags, getStations, getStation} = require('./lib/data');
 const app = express();
 
 const port = 1234;
 
 app.use(cors());
 
-const player = new MPlayer();
+const player = new Player();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/tags', (req, res) => {
   console.log('getting tags');
-  const tags = res.send(getUniqueTags());
+  res.send(getUniqueTags());
+});
+
+app.get('/status', (req, res) => {
+  res.send(player.status);
 });
 
 app.get('/stations', (req, res) => {
@@ -26,7 +30,7 @@ app.get('/play', (req, res) => {
   const {stationId} = req.query;
   const station = getStation(stationId);
   console.log(`playing ${station.name}: ${station.stream}`);
-  player.setSource(station.stream);
+  player.playStation(station);
   res.send();
 });
 
@@ -39,24 +43,3 @@ app.get('/stop', (req, res) => {
 app.listen(port, () => {
   console.log(`Radio server listening on port ${port}!`);
 });
-
-const getUniqueTags = () => {
-  const stationData = require('./config.json');
-  const allTags = stationData.map(data => data.tags).flat();
-  const uniqueTags = allTags.filter(
-    (item, index) => allTags.indexOf(item) === index,
-  );
-  return uniqueTags.sort();
-};
-
-const getStations = queryParams => {
-  const stationData = require('./config.json').map((data, index) => {
-    return {...data, id: index};
-  });
-  if (queryParams.tag) {
-    return stationData.filter(data => data.tags.includes(queryParams.tag));
-  }
-  return stationData;
-};
-
-const getStation = id => require('./config.json')[id];
