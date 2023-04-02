66ce6c22c9deac5ec6d19a7280b1f8290742efc5
Paul Martin <paul.y.martin@johnlewis.co.uk>
2023-04-02 15:18:34 +0100

    update startup script

--- startup-radio-tmux.sh
+++ startup-radio-tmux.sh
@@ -9,9 +9,9 @@ tmux new-session -d -s $SESSION -n 'Main'
 tmux send-keys -t 'Main' 'clear' C-m
 
 tmux new-window -t $SESSION -n 'Back end' -c 'back-end'
-tmux send-keys -t 'Back end' 'clear' C-m 'yarn start' C-m
+tmux send-keys -t 'Back end' 'clear' C-m 'npm start' C-m
 
-tmux new-window -t $SESSION -n 'Front end' -c 'front-end'
-tmux send-keys -t 'Front end' 'clear' C-m 'yarn serve' C-m
+tmux new-window -t $SESSION -n 'Front end' -c 'front-end-2'
+tmux send-keys -t 'Front end' 'clear' C-m 'npm start' C-m
 
 tmux select-window -t 'Main'
