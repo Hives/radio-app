14e8393543abea6aff7fb7bf9fc2ea0fa1bd870b
Paul Martin <paul.y.martin@johnlewis.co.uk>
2023-04-02 14:50:01 +0100

    fix volume

--- front-end-2/src/components/Status/Playing/Volume.tsx
+++ front-end-2/src/components/Status/Playing/Volume.tsx
@@ -1,31 +1,46 @@
-import { ChangeEvent } from "react";
-import { setVolume } from "@/radio/radio";
+import { ChangeEvent, useEffect, useState } from "react";
+import { setVolume as setServerVolume } from "@/radio/radio";
 import { VolumeButton } from "@/components/Status/Playing/VolumeButton";
 
 type Props = {
   level: number;
 };
 
-export const Volume = ({ level }: Props) => (
-  <div className="w-full">
-    <div className="flex w-full gap-2">
-      <VolumeButton disabled={level <= 0} onClick={() => setVolume(level - 1)}>
-        -
-      </VolumeButton>
-      <input
-        className="flex-grow"
-        type="range"
-        name="volume"
-        min="0"
-        max="100"
-        value={level}
-        onChange={(e: ChangeEvent<HTMLInputElement>) =>
-          setVolume(parseInt(e.target.value))
-        }
-      />
-      <VolumeButton disabled={level >= 100} onClick={() => setVolume(level + 1)}>
-        +
-      </VolumeButton>
+export const Volume = ({ level }: Props) => {
+  const [localLevel, setLocalLevel] = useState(level);
+
+  useEffect(() => {
+    setServerVolume(localLevel);
+  }, [localLevel]);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full gap-2">
+        <VolumeButton
+          disabled={localLevel <= 0}
+          onClick={() => setLocalLevel(localLevel - 1)}
+        >
+          -
+        </VolumeButton>
+        <input
+          className="flex-grow"
+          type="range"
+          name="volume"
+          min="0"
+          max="100"
+          value={localLevel}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setLocalLevel(parseInt(e.target.value))
+          }
+        />
+        {localLevel}
+        <VolumeButton
+          disabled={localLevel >= 100}
+          onClick={() => setLocalLevel(localLevel + 1)}
+        >
+          +
+        </VolumeButton>
+      </div>
     </div>
-  </div>
-);
+  );
+};
